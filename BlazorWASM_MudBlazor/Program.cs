446a32dc23e9fd247777dd6c55f804a18065d585
abel <abel>
2024-01-12 15:09:10 +0100

    Add project files.

---
+++ BlazorWASM_MudBlazor/Program.cs
@@ -0,0 +1,15 @@
+using BlazorWASM_MudBlazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//Import service
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
